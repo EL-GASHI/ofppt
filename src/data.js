b2342866e3c1f5de5930e97410460415ba8cecbc
asmaebssr <146130950+asmaebssr@users.noreply.github.com>
2024-01-19 16:34:53 +0100

    Update data.js
    add new sectors

--- src/data.js
+++ src/data.js
@@ -6,6 +6,31 @@ const sectors = [
         system: 'Cours du jour',
         get: 'Diplomante',
         tronc: true,
-        link: 'https://www.myway.ac.ma/fr/filiere/DIA_DEV_TS'
+        link: 'https://www.myway.ac.ma/fr/filiere/DIA_DEV_TS',
+        models: [
+            "oop kendoussi",
+            "oop kendoussi",
+            "oop kendoussi",
+            "oop kendoussi",
+            "oop kendoussi",
+            "oop kendoussi"
+            ]
+    },
+      {
+        name: 'Développement Digital',
+        desc: 'Le tronc commun en Développement Digital est une étape importante pour acquérir les bases nécessaires à l\'étude, la conception, la construction',
+        type: 'Technicien Spécialisé',
+        system: 'Cours du jour',
+        get: 'Diplomante',
+        tronc: true,
+        link: 'https://www.myway.ac.ma/fr/filiere/DIA_DEV_TS',
+        models: [
+            "oop kendoussi",
+            "oop kendoussi",
+            "oop kendoussi",
+            "oop kendoussi",
+            "oop kendoussi",
+            "oop kendoussi"
+            ]
     },
 ]
